0fdfd26d6fde7dbbc3abc114ff2ae867ca93f5a6
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-03-01 17:46:47 +0530

    Initial commit

---
+++ md2docx.py
@@ -0,0 +1,62 @@
+import argparse
+import StringIO
+import sys
+
+from docx import Document
+from docx.shared import Inches
+import mistune
+
+class DocXMarkdown(mistune.Markdown):
+    def parse(self, text):
+        s = StringIO.StringIO()
+        ret = super(DocXMarkdown, self).parse(text)
+        self.renderer.doc.save(s)
+        return s.getvalue()
+        
+
+class DocXRenderer(mistune.Renderer):
+    def __init__(self, *largs, **kargs):
+        self.doc = Document()
+        self.clist = None
+        super(DocXRenderer, self).__init__(*largs, **kargs)
+
+    def list_item(self, text):
+        return "{}\n".format(text)
+      
+    def list(self, body, ordered=True):
+        if ordered:
+            style = "ListNumber3"
+        else:
+            style = "ListBullet"
+        for i in body.rstrip().split("\n"):
+            self.doc.add_paragraph(i, style)
+        return ''
+        
+    def header(self, text, level, raw=None):
+        self.doc.add_heading(text, level)
+        return ''
+
+    def paragraph(self, text):
+        self.doc.add_paragraph(text, style=None)
+        return ''
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Converts a markdown document into docx")
+    parser.add_argument("input", help="Markdown input file")
+    parser.add_argument("output", help="docx output file (will overwrite if it already exists)")
+    args  = parser.parse_args()
+    return args
+                            
+
+def main():
+    renderer = DocXRenderer()
+    markdown = DocXMarkdown(renderer = renderer)
+    args = parse_args(sys.argv[1:])
+    with open(args.input) as f:
+        with open(args.output, "wb") as g:
+            g.write(markdown(f.read()))
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ sample.md
@@ -0,0 +1,29 @@
+# This is a title
+
+Some text here. Some more here
+
+Here are some of *my* items
+
+ * Blathering
+ * Blatherskite
+ * And others
+
+## This is a subtitle
+
+Action!
+Here are my things
+
+  1. First one
+  1. Second one
+  1. Third one
+  1. Fourth one
+
+
+## And another
+
+
+# Back to to the top again
+
+Some text
+
+
